b0bc6c106c50a68c63b3e5ae23cb6afc0b24ea37
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-04-08 11:48:32 -0300

    Add files via upload

--- SQL/atividade aula 23.sql
+++ SQL/atividade aula 23.sql
@@ -9,7 +9,12 @@ join sociologia on SOCIOLOGIA.numero_registro  = livro.numero_registro;
 
 --2. Listar os nomes dos funcionários que são bibliotecários.
 SELECT nome FROM FUNCIONARIO where matricula like 'B%';
+
 SELECT nome FROM BIBLIOTECARIO;
+
+select FUNCIONARIO.nome from FUNCIONARIO
+join BIBLIOTECARIO on
+FUNCIONARIO.matricula = BIBLIOTECARIO.matricula;
 
 
 --3. Exibir os títulos dos livros publicados antes de 2000.
@@ -17,9 +22,13 @@ SELECT titulo FROM Livro WHERE Ano_Publicacao < 2000;
 
 
 --4. Mostrar o número total de livros em cada biblioteca.
+select cnpj,COUNT(*) as total_livro
+from LIVRO_BIBLIOTECA
+group by cnpj
+
 select * from LIVRO_BIBLIOTECA
 join LIVRO on LIVRO.numero_registro = LIVRO_BIBLIOTECA.numero_registro
-where cnpj = '01234567000112'
+--where cnpj = '01234567000112'
 
 insert into LIVRO_BIBLIOTECA
 values
@@ -76,6 +85,7 @@ values
 
 
 
+
 DELETE FROM LIVRO_BIBLIOTECA WHERE numero_registro >= 1
 
 --5. Listar os eventos do tipo 'Workshop' que ocorreram após 2020.
@@ -89,20 +99,37 @@ SELECT nome FROM usuario
 join EMPRESTIMO on EMPRESTIMO.usuario = usuario.id_usuario
 where year(data_emprestimo) = 2023 and MONTH(data_emprestimo) = 01
 
+select * from EMPRESTIMO
 
 
 --7. Retornar os títulos dos livros de tecnologia que têm 'Python' no título.
 SELECT titulo FROM Livro where Titulo LIKE '%Python%';
 
+SELECT titulo FROM Livro
+join TECNOLOGIA on livro.numero_registro = TECNOLOGIA.numero_registro
+where Titulo LIKE '%Python%';
+
 
 --8. Listar os títulos dos periódicos disponíveis na biblioteca com CNPJ '12345678000123'.
-SELECT * FROM PERIODICO
+SELECT titulo FROM PERIODICO
 join PERIODICO_BIBLIOTECA on PERIODICO_BIBLIOTECA.numero_registro  = PERIODICO.numero_registro
 where CNPJ = '12345678000123';
 
 
 --9. Mostrar os nomes dos funcionários que não são técnicos de TI.
 SELECT nome FROM FUNCIONARIO where matricula not like 'T00%';
+
+select * from FUNCIONARIO
+where matricula not in (
+	select matricula from TECNICO_TI
+);
+
+
+--9.1 todos os livros que não são de tecnologia
+SELECT * FROM Livro
+where numero_registro not in (
+	select numero_registro from TECNOLOGIA 
+);
 
 
 --10.Exibir o título e o autor dos livros emprestados pelo usuário com ID 'U0001'.
@@ -111,10 +138,13 @@ join EMPRESTIMO on EMPRESTIMO.livros = livro.numero_registro
 where usuario = 'U0001'
 
 
-
 --11.Listar todas as palestras que custaram mais de R$ 100,00.
 SELECT * FROM EVENTO where
 tipo = 'PALESTRA' and custo > 100.00;
+
+SELECT * FROM EVENTO 
+join PALESTRA on EVENTO.id_evento = PALESTRA.id_evento
+where custo > 100.00;
 
 
 --12.Retornar os títulos dos livros de ciência que foram publicados após 2010.
@@ -130,6 +160,10 @@ SELECT * FROM ATENDENTE where nome like '%maria%'
 --14.Mostrar os títulos dos livros que foram emprestados mais de 5 vezes.
 select * from EMPRESTIMO
 
+select LIVRO.titulo, COUNT(titulo) as total_emprestimo from LIVRO
+join EMPRESTIMO on LIVRO.numero_registro = EMPRESTIMO.livros
+where (select COUNT(*) from EMPRESTIMO where livro.numero_registro = EMPRESTIMO.livros) >= 1
+group by titulo
 
 
 --15.Listar os nomes dos usuários que emprestaram livros de tecnologia.
@@ -146,22 +180,56 @@ tipo = 'palestra' and year(data) = 2022;
 
 --17.Retornar os títulos dos periódicos que têm 'Saúde' no título.
 SELECT * FROM PERIODICO where titulo like '%saúde%';
+SELECT titulo FROM PERIODICO where titulo like '%saúde%';
 
 
 --18.Listar os nomes dos estagiários que começam com a letra 'L'.
 SELECT * FROM ESTAGIARIO where nome like 'l%';
+SELECT nome FROM ESTAGIARIO where nome like 'l%';
 
 
 --19.Mostrar os títulos dos livros de sociologia que foram publicados nos anos 90.
 SELECT titulo FROM LIVRO 
 join sociologia on SOCIOLOGIA.numero_registro  = livro.numero_registro
-WHERE Ano_Publicacao < 2000;
+WHERE ano_publicacao > 1900 and Ano_Publicacao < 2000 ;
 
 
 --20.Exibir os títulos dos livros e os nomes dos autores dos livros emprestados em fevereiro de 2023.
+
+CREATE VIEW vm_livros_emprestados
+as
+select titulo, autor from LIVRO
+join EMPRESTIMO on EMPRESTIMO.livros = livro.numero_registro
+where year(data_emprestimo) = 2023 and MONTH(data_emprestimo) = 02;
+
+select * from vm_livros_emprestados;
+
 select * from LIVRO
 join EMPRESTIMO on EMPRESTIMO.livros = livro.numero_registro
 where year(data_emprestimo) = 2023 and MONTH(data_emprestimo) = 02
+
+
+
+--calcule a media de preços dos eventos do tipo workshop
+create view media_preco_workshop
+as
+SELECT round(avg(custo),2) AS media_preco FROM EVENTO
+where tipo = 'workshop'
+--join WORKSHOP on WORKSHOP.id_evento = EVENTO.id_evento
+
+select * from media_preco_workshop
+
+-- soma de todos os eventos que aconteceram apartis de 2022
+select round(sum(custo),2) as soma_preco
+from EVENTO
+where year(data) >= 2022
+
+
+count  --contar
+avg    --média
+sum    --soma
+
+
 
 
 
